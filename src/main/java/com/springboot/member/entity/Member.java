1b798db1aa1ed3b9620a16a5777959a833db1742
pyyeon <pyyeon98@gmail.com>
2024-08-16 16:23:25 +0900

    asd

--- src/main/java/com/springboot/member/entity/Member.java
+++ src/main/java/com/springboot/member/entity/Member.java
@@ -46,10 +46,7 @@ public class Member {
     @OneToMany(mappedBy = "member", cascade = CascadeType.MERGE)
     private List<Interpretation> interpretations = new ArrayList<>();
 
-
-
-
-    // TODO 추가 된 부분
+    
     public enum MemberStatus {
         MEMBER_ACTIVE("활동중"),
         MEMBER_SLEEP("휴면 상태"),
