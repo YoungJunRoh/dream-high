ed76f8d18fd16f05c6de22b2aec6ab0920f1afcf
YoungJunRoh <shdudwns59@gmail.com>
2024-08-21 11:13:04 +0900

    Logout Feature

--- src/main/java/com/springboot/email/dto/EmailRequestDto.java
+++ src/main/java/com/springboot/email/dto/EmailRequestDto.java
@@ -1,15 +1,15 @@
-package com.springboot.email.dto;
-
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-
-@Getter
-@Setter
-public class EmailRequestDto {
-    @Email
-    @NotEmpty
-    private String email;
-}
+//package com.springboot.email.dto;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//import javax.validation.constraints.Email;
+//import javax.validation.constraints.NotEmpty;
+//
+//@Getter
+//@Setter
+//public class EmailRequestDto {
+//    @Email
+//    @NotEmpty
+//    private String email;
+//}
