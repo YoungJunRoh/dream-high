c630b794c93271e0178dd9e06bcd80aa74542d07
YoungJunRoh <shdudwns59@gmail.com>
2024-08-26 13:48:53 +0900

    DreamActive Get

--- src/main/java/com/springboot/dream/repository/DreamRepository.java
+++ src/main/java/com/springboot/dream/repository/DreamRepository.java
@@ -5,8 +5,15 @@ import com.springboot.dream.entity.Dream;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DreamRepository extends JpaRepository<Dream, Long> {
     Page<Dream> findByDreamKeywords_NameContaining(String keyword, Pageable pageable);
 
+    Page<Dream> findByDreamStatus(Dream.DreamStatus status, Pageable pageRequest);
+
+    // 드림 상태가 ACTIVE인 드림을 필터링하고, 드림 키워드를 기반으로 검색하는 메소드
+    // 키워드가 비어있으면 전체 드림을 반환
+    Page<Dream> findByDreamStatusAndDreamKeywords_NameContaining(Dream.DreamStatus status, String keyword, Pageable pageRequest);
 }
