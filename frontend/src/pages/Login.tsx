4f776dc304025702f824faf521b02f144f3b9d81
pyyeon <pyyeon98@gmail.com>
2024-08-28 20:41:51 +0900

    empty

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,41 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import '../styles/login.css';
 import ResultBigBox from '../components/BigBox.tsx';
 import ResultSmallBox from '../components/SmallBox.tsx';
 import Button from '../components/Button.tsx';
-import { LoginResponse, memberApiResponse } from '../interfaces/member.ts'
-import { getMember, postLogin } from '../services/MemberService.ts';
+import { LoginResponse } from '../interfaces/member.ts'
+import { postLogin } from '../services/MemberService.ts';
 import { useMember } from '../hooks/MemberManager.tsx';
-import { MemberContextType } from '../hooks/MemberManager.tsx';
-import { MemberManager } from '../hooks/MemberManager.tsx';
 import { Link, useNavigate } from 'react-router-dom';
-import Swal from 'sweetalert2'; 
+import Swal from 'sweetalert2'; // Swal 추가
 import Footer from '../components/Footer.tsx';
 import Input from '../components/Input.tsx';
-import styled from 'styled-components';
-import googleIcon from '../assets/img-google.svg';
-import happycat from '../assets/happycat.gif';
-
-const GoogleOAuth = styled.div`
-    display: flex;
-    flex-direction: column;
-    background-image: url(${googleIcon});
-    background-repeat: no-repeat;
-    background-size: contain;
-    width: 250px;
-    height: 60px;
-    cursor: pointer;
-`;
 
 const Login = () => {
-    const { setAuthorization, setRefresh, setLogin, setName, setProfileUrl} = useMember();
-    
+    const { setAuthorization, setRefresh, setLogin } = useMember();
     const navigate = useNavigate();
+
     const [response, setResponse] = useState<LoginResponse | null>(null);
     const [email, setEmail] = useState<string>();
     const [password, setPassword] = useState<string>();
 
-   
     // 이메일 추출
     const emailHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setEmail(e.target.value);
@@ -65,54 +48,31 @@ const Login = () => {
 
     // 로그인 처리
     const loginHandler = async () => {
-        if (email !== undefined && password != undefined) {
-            const response = await postLogin(email as string, password as string);
-            if (response.status === 401) {
-                Swal.fire({
-                    title: '다시 입력하라냥',
-                    html: ` <p>이메일 또는 비밀번호가 잘못되었다냥 ㅇㅅㅇ</p> <img src="${happycat}" alt="Happy Cat" style="width: 300px; height: auto; margin-bottom: 10px;" />
-                    `,
-                    confirmButtonText: '확인'
-                });
-                return;
-            } else if (response.status === 500) {
-                Swal.fire({
-                    title: '로그인 중 문제가 발생했다옹 ',
-                    html: `<img src="${happycat}" alt="Happy Cat" style="width: 300px; height: auto; margin-bottom: 10px;" />
-                     <p>다시 시도해라냥 ㅇㅅㅇ</p>
-                    
-                `,
-                    confirmButtonText: '확인'
-                });
-                return;
-            } else {
-                setResponse(response.data);
-                 // 로그인 성공 시 토큰 저장 및 페이지 이동
-                setAuthorization(response.headers.authorization);
-                setRefresh(response.headers.refresh);
-                setLogin(true);
-                navigate('/');
-            }
-        } else if (email === undefined) {
+        const response = await postLogin(email as string, password as string);
+        setResponse(response.data);
+        console.log("Logging in with:", { email, password });
 
+        // 로그인 성공 시 토큰 저장 및 페이지 이동
+        setAuthorization(response.headers.authorization);
+        setRefresh(response.headers.refresh);
+        setLogin(true);
+        navigate('/');
+      
+        // 예외 처리
+        if (response.status === 401) {
             Swal.fire({
                 text: '이메일 또는 비밀번호가 잘못되었다냥 ㅇㅅㅇ',
-                html: `<img src="${happycat}" alt="Happy Cat" style="width: 300px; height: auto; margin-bottom: 10px;" />
-
-            `,
+                icon: 'error',
                 confirmButtonText: '확인'
             });
-            return;
-        } else if (password === undefined) {
+        } else if(response.status === 500){
             Swal.fire({
                 text: '로그인 중 문제가 발생했서 다시 시도해라냥 ㅇㅅㅇ',
-                html: `<img src="${happycat}" alt="Happy Cat" style="width: 300px; height: auto; margin-bottom: 10px;" />
-                <p>로그인해야 공유할 수 있다냥.</p>
-            `,
+                icon: 'error',
                 confirmButtonText: '확인'
             });
-            return;
         }
+
     };
 
     return (
@@ -133,7 +93,7 @@ const Login = () => {
                         $w_fontSize='20px'
                         type='email'
                     ></Input>
-               </div>
+                </div>
                 <div className='login-input'>
                     <h5>비밀번호</h5>
                     <Input
@@ -150,14 +110,12 @@ const Login = () => {
                     ></Input>
                 </div>
             </ResultBigBox>
-            <div className= 'logBtn'>
             <Button
                 name='로그인🐾'
                 mode='login'
                 draggable={true}
                 onClick={loginHandler}
             />
-            <div className='loginoption'>
             <Button
                 name='SNS로그인'
                 mode='pass'
@@ -178,9 +136,8 @@ const Login = () => {
                     draggable={true}
                 />
             </Link>
-            </div>
-            </div>
             <div id='blank'></div>
+            <Footer />
         </div>
     );
 }
